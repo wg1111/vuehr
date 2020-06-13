b6ea08b30b7b3e34d4a726ad038bb16eedf28496
wg1111 <875924452@qq.com>
2020-06-13 20:44:50 +0800

    操作员管理开发和登录页加载数据提醒

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        <el-form :rules="rules" :model="loginForm" ref="loginForm" class="loginContainer">
+        <el-form
+                :rules="rules"
+                :model="loginForm"
+                ref="loginForm"
+                v-loading="loading"
+                element-loading-text="登录中..."
+                element-loading-spinner="el-icon-loading"
+                class="loginContainer">
             <h3 class="loginTitle">系统登录</h3>
             <el-form-item prop="username">
                 <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
@@ -8,7 +15,7 @@
             <el-form-item prop="password">
                 <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
             </el-form-item>
-            <el-checkbox v-model="checked" class="loginRemember"></el-checkbox>
+            <el-checkbox v-model="checked" class="loginRemember">记住密码</el-checkbox>
             <el-button type="primary" style="width: 100%" @click="login">登录</el-button>
         </el-form>
     </div>
@@ -25,6 +32,7 @@
                     password:"123"
                 },
                 checked:true,
+                loading: false,//加载图案
                 rules:{
                     username:[{required:true,message:"请输入用户名",trigger:"blur"}],
                     password:[{required:true,message:"请输入密码",trigger:"blur"}]
@@ -35,7 +43,9 @@
             login(){
                 this.$refs.loginForm.validate((valid) => {
                     if (valid) {
+                        this.loading = true;
                         this.postKeyValue('/doLogin',this.loginForm).then(resp=>{
+                            this.loading = false;
                             if (resp){
                                 // alert(JSON.stringify(resp))
                                 window.sessionStorage.setItem("user",JSON.stringify(resp.obj))
--- src/views/sys/SysHr.vue
+++ src/views/sys/SysHr.vue
@@ -1,15 +1,204 @@
 <template>
     <div>
-        操作员管理
+        <div class="searchHeader">
+            <el-input size="small" style="width: 400px" prefix-icon="el-icon-search" placeholder="请输入用户名查询用户..." v-model="keywords"
+            @keydown.enter.native="search"></el-input>
+            <el-button size="small" icon="el-icon-search" type="primary" style="margin-left: 8px" @click="search">搜索</el-button>
+        </div>
+        <div class="hrContainer">
+            <el-card class="box-card" v-for="(hr,index) in hrs" :key="index">
+                <div slot="header" class="clearfix">
+                    <span>{{hr.name}}</span>
+                    <el-button  size="small" style="float: right; padding: 3px 0" type="danger" icon="el-icon-delete" @click="doDelete(hr)"></el-button>
+                </div>
+                <div>
+                    <div class="img_container">
+                        <img :src="hr.userface" :alt="hr.name" :title="hr.name" class="userface">
+                    </div>
+                    <div class="content_container">
+                        <div>用户名：{{hr.name}}</div>
+                        <div>手机号码：{{hr.telephone}}</div>
+                        <div>电话号码：{{hr.phone}}</div>
+                        <div>地址：{{hr.address}}</div>
+                        <div>用户状态：
+                            <el-switch
+                                v-model="hr.enabled"
+                                active-text="启用"
+                                inactive-text="禁用"
+                                @change="enabledChange(hr)"
+                                active-color="#13ce66"
+                                inactive-color="#ff4949">
+                        </el-switch>
+                            </div>
+                        <div>用户角色：
+                            <el-tag v-for="(role,index1) in hr.roles" :key="index1" type="success" size="small" style="margin-right: 4px">
+                                {{role.nameZh}}
+                            </el-tag>
+                            <el-popover
+                                    placement="right"
+                                    title="角色列表"
+                                    @show="showPopover(hr)"
+                                    @hide="hidePopover(hr)"
+                                    width="200"
+                                    trigger="click">
+                                <el-select v-model="selectedRoles" multiple placeholder="请选择" size="small">
+                                    <el-option
+                                            v-for="(role,indexj) in allRoles"
+                                            :key="indexj"
+                                            :label="role.nameZh"
+                                            :value="role.id">
+                                    </el-option>
+                                </el-select>
+                                <el-button slot="reference" size="mini" icon="el-icon-more" type="text"></el-button>
+                            </el-popover>
+                           </div>
+                        <div>备注：{{hr.remark}}</div>
+                    </div>
+                </div>
+            </el-card>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "SysHr"
+        name: "SysHr",
+        data() {
+            return {
+                keywords: "",
+                hrs: [],
+                hr: [],
+                allRoles: [],
+                selectedRoles: []//存放的是已选择的角色id
+            };
+        },
+        mounted() {
+            this.showAllHrs();
+        },
+        methods:{
+            doDelete(hr) {
+                this.$confirm('此操作将永久删除操作员【'+hr.name+'】, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.deleteRequest("/system/hr/"+hr.id).then(resp=>{
+                        if (resp) {
+                            this.showAllHrs();
+                        }
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            search(){
+                this.showAllHrs();
+            },
+            hidePopover(hr) {
+                //角色有更新才执行更新操作，否则不执行
+                let roles =[];
+                Object.assign(roles,hr.roles);
+                let flag = false;
+                if (roles.length !== this.selectedRoles.length) {
+                    flag = true;
+                }else {
+                    for (let i = 0; i < roles.length; i++) {
+                        let role = roles[i];
+                        for (let j = 0; j < this.selectedRoles.length; j++) {
+                            let selectedRole = this.selectedRoles[j];
+                            if (role.id === selectedRole) {
+                                roles.splice(i, 1);
+                                i--;
+                                break;
+                            }
+                        }
+                        }
+                    if (roles.length !== 0) {
+                        flag = true;
+                    }
+                }
+                if (flag) {
+                    let url = "/system/hr/updateRole?hrid=" + hr.id;
+                    this.selectedRoles.forEach(rid => {
+                        url += '&rids=' + rid;
+                    });
+                    this.putRequest(url).then(resp => {
+                        if (resp) {
+                            this.showAllHrs();
+                        }
+                    });
+                }
+
+            },
+            showPopover(hr) {
+                this.getAllRoles();
+                let roles = hr.roles;
+                this.selectedRoles = [];
+                roles.forEach(role => {
+                    this.selectedRoles.push(role.id);
+                });
+            },
+            getAllRoles(){
+                this.getRequest("/system/hr/roles").then(resp => {
+                    if (resp) {
+                        this.allRoles = resp;
+                    }
+                });
+            },
+            enabledChange(hr) {
+                delete hr.roles;
+                this.putRequest("/system/hr/",hr).then(resp=>{
+                    if (resp) {
+                        this.showAllHrs();
+                    }
+                })
+                console.log(hr)
+            },
+            showAllHrs() {
+                this.getRequest("/system/hr/?keywords="+this.keywords).then(resp=>{
+                    if (resp)
+                        this.hrs = resp;
+                })
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .content_container{
+        margin-top: 20px
+    }
+    .content_container div{
+        font-size: 12px;
+        color: #409eff;
+    }
+    .img_container{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+.searchHeader{
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+}
+    .box-card{
+        width: 350px;
+        margin-bottom: 20px;
+    }
+    .hrContainer{
+        margin-top: 25px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around
+    }
 
+    .userface{
+        width: 72px;
+        height: 72px;
+        border-radius: 72px;
+    }
 </style>
